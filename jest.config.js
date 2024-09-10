56354a656e0f9182759f8cc5e32c7ffb75dfbe5f
Gabriel <147352311+patzsl@users.noreply.github.com>
2024-09-09 23:25:20 -0300

    ci: add test job and update checkout action to v3 in deploy workflow
    - Added a new `test` job to the GitHub Actions deploy workflow.
    - Updated `actions/checkout` to version 3 in all jobs.
    - Configured Node.js setup and caching in the `test` job.
    - Added steps to install dependencies and run tests in the `test` job.
    - Made the `build` job dependent on the `test` job.
    
    test: add moduleNameMapper configuration in jest.config.js
    
    - Added `moduleNameMapper` configuration to `jest.config.js` for path aliasing.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/src/setup.jest.ts"],
+  moduleNameMapper: {
+    "^@shared/(.*)$": "<rootDir>/src/app/shared/$1",
+    "^@core/(.*)$": "<rootDir>/src/app/core/$1",
+    "^@models/(.*)$": "<rootDir>/src/app/shared/models/$1",
+    "^@mocks/(.*)$": "<rootDir>/src/mocks/$1",
+    "^@environments/(.*)$": "<rootDir>/src/environments/$1",
+  },
 };
